d33a64c49d6084c7d4428e39a42c612ae1cdf2ff
Terri Riggle <codeofarmour@outlook.com>
2017-09-30 06:49:22 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
-  if speed == > 40 || < 60
-	 false
+	if speed <= 40 || speed >= 60
+		true
 	else
-	 true
+		false
+	end
 end
 
 
 
-
 def not_safe?(speed)
-
+	speed  <= 40 || speed >= 60 ? true : false
 end
